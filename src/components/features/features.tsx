72229c456867dce4d40815cfad7ae27fd090b3ad
Frioui <frioui@MacBook-Air-de-Frioui.local>
2025-06-11 14:59:30 +0200

    refactor: update loading state to true

--- src/components/features/features.tsx
+++ src/components/features/features.tsx
@@ -10,7 +10,7 @@ interface FeaturesProps {
 }
 
 const Features: FC<FeaturesProps> = ({ filteredFeatures }) => {
-  const isLoading = false;
+  const isLoading = true;
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
       {isLoading
