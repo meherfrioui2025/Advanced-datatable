e14392222ff5898a0815b7fecf793e3b83071353
Frioui <frioui@MacBook-Air-de-Frioui.local>
2025-06-23 18:44:28 +0200

    style: fix style for input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -5,7 +5,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement>;
 const Input: FC<InputProps> = ({ className = "", ...props }) => {
   return (
     <input
-      className={`w-full h-10 px-3 py-2 text-base border rounded-md bg-white border-gray-300 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      className={`w-full h-10 px-3 py-2 text-base border rounded-md bg-white border-gray-300 placeholder-gray-400 focus:outline-none ${className}`}
       {...props}
     />
   );
